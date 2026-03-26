8b1aab95e2b42182af75b9510bb2fce81f9bf54f
Student <student@tournament-platform.com>
2026-03-26 12:10:23 +0200

    Update build.sh: add automatic superuser creation

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if env variables are present
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser \
+        --no-input \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" || true
+fi
